de7b42d1a291ce72c054fa6391e9d47438e2ce30
Ayushguptaa22 <Ayushgupta_19021785.cse@geu.ac.in>
2022-08-18 12:55:15 +0530

    styling setup

--- papertechy/pages/index.js
+++ papertechy/pages/index.js
@@ -1,9 +1,25 @@
 import React from 'react'
 
-const index = () => {
+const Home = () => {
   return (
-    <div>index</div>
+    <>
+    Main Page Banner
+    
+    <div className="products-heading">
+      <h2>Best selling products</h2>
+      <p>Variety of products from Paper quilling artworks to custom resin badges and more ...</p>
+    </div>
+    
+    <div className='products-container'>
+     { ['Product-1 ','Product-2 ','Product-3 ', 'Product-4 ', 'Product-5 '].map(
+       (product) => product)}
+    </div>
+
+    <div className='Footer'>
+      <h3>Footer</h3>
+    </div>
+    </>
   )
 }
 
-export default index
+export default Home
