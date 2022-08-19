5cf46b1d23726b35543567b2d199dc794f3b730f
Ayushguptaa22 <Ayushgupta_19021785.cse@geu.ac.in>
2022-08-19 21:13:13 +0530

    Banner edit

--- papertechy/components/HeroBanner.jsx
+++ papertechy/components/HeroBanner.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
-
+import Link from 'next/link';
 const HeroBanner = () => {
   return (
-    <div>HeroBanner</div>
+    <div className='hero-banner-container'>
+      <div>
+        <p className=''>Small Text</p>
+        <h3>Medium Text</h3>
+        <img src='' alt='' className='hero-banner-image'/>
+
+        <div>
+          <Link href="/product/ID">
+            <button type="button">Button text</button>
+          </Link>
+          <div className='desc'>
+            <h5>Description</h5>
+            <p>DESCRIPTION</p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
